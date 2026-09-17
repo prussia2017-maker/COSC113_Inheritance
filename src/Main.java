5e5e313c3750c4bade615a0b350013caa38a5a37
Artur <Prussia2017@gmail.com>
2026-09-17 09:32:32 -0400

    Inheritance_1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Create two person object
+        Person p1 = new Person();
+
+        Person p2 = new Person(72, 70, "Black", "Barack Obama", 170);
+
+        System.out.println(p2.name);
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    // height, age, color, name, weight: Instance variable
+
+    double height;
+
+    int age;
+
+    String color;
+
+    String name;
+
+    double weight;
+
+    Person(){
+        // Default constructor can be empty
+        height = 48;
+        age = 18;
+        color = "black";
+        name = "";
+        weight = 140;
+    }
+
+    Person(double height, int age, String color, String name, double weight){
+        // Parameters are local variables to the constructor
+        this.height = height;
+        this.age = age;
+        this.color = color;
+        this.name = name;
+        this.weight = weight;
+    }
+}
