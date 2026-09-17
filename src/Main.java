6bf3b9017d3d5b9df78d739192a8b2005cde9681
Artur <Prussia2017@gmail.com>
2026-09-17 10:21:57 -0400

    Inheritance_2

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ public class Main {
         // Create two person object
         Person p1 = new Person();
 
-        Person p2 = new Person(72, 70, "Black", "Barack Obama", 170);
-
+    //    Person p2 = new Person(72, 70, "Black", "Barack Obama", 170);
+        //   System.out.println(p2.name);
+        Person p2 = new Person(72, 70);
         System.out.println(p2.name);
+
+        Person p3 = p2.Call_Private();
 
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+//
 public class Person {
     // height, age, color, name, weight: Instance variable
 
@@ -11,7 +12,8 @@ public class Person {
 
     double weight;
 
-    Person(){
+    // Constructors can have private, protected, public access mofifiers
+    public Person(){
         // Default constructor can be empty
         height = 48;
         age = 18;
@@ -19,13 +21,29 @@ public class Person {
         name = "";
         weight = 140;
     }
+//Protected members are accessible within the same package
+    protected Person(double height, int age) {
+        // Parameters are local variables to the constructor
+        this.height = height;
+        this.age = age;
 
-    Person(double height, int age, String color, String name, double weight){
+
+    }
+    private Person(double height, int age, String color, String name, double weight) {
         // Parameters are local variables to the constructor
         this.height = height;
         this.age = age;
         this.color = color;
         this.name = name;
         this.weight = weight;
+
+    }
+
+        // Setter and Getter
+        //Methods
+
+        public Person Call_Private(){
+        Person p3 = new Person(72, 70, "Black", "Barack Obama", 170);
+        return p3;
     }
 }
