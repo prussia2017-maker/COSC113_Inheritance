456fae72afdae23e21ac7453f9038b31b424e062
Artur <Prussia2017@gmail.com>
2026-09-17 17:17:11 -0400

    Inheritance_Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// https://github.com/prussia2017-maker/COSC113_Inheritance
 public class Main {
     public static void main(String[] args) {
         // Create two person object
